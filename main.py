7f13f73e440ccf4d36b9aa4afa397304189bf753
lourdes-privitera <lulitaprivi@gmail.com>
2026-06-09 21:58:38 -0300

    Agrego el llamado a las funciones

---
+++ main.py
@@ -0,0 +1,24 @@
+#liampiar..solo debe quedar la ejecucion del programa
+from inputs import (
+ingresar_uso_cpu,
+ingresar_uso_ram,
+ingresar_espacio_libre,
+ingresar_usuarios_conectados,
+ingresar_procesos_activos,
+ingresar_sistema_operativo,
+ingresar_estado_firewall,
+ingresar_tipo_servidor
+)
+
+print("------Sistema de Diagnóstico y Configuración de Servidor Basado en Reglas------")
+#ingresar numeros 
+uso_cpu = ingresar_uso_cpu
+uso_ram = ingresar_uso_ram
+espacio_libre = ingresar_espacio_libre
+usuarios_conectados = ingresar_usuarios_conectados
+procesos_activos = ingresar_procesos_activos
+
+#ingresar opciones EUTILIZACION DE LA FUNCION POR PARAMETROS PARA PEDIR OPCIONES CERRADAS
+sistema_operativo = ingresar_sistema_operativo
+estado_firewall = ingresar_estado_firewall
+tipo_servidor = ingresar_tipo_servidor
