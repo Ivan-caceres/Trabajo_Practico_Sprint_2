618d1622cf8d931d9e93f3062d22814cd45f94e8
lourdes-privitera <lulitaprivi@gmail.com>
2026-06-09 23:44:47 -0300

    Agrego los llamados a las funciones

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ ingresar_usuarios_conectados,
 ingresar_procesos_activos,
 ingresar_sistema_operativo,
 ingresar_estado_firewall,
-ingresar_tipo_servidor
+ingresar_tipo_servidor,
+ingresar_nombre_servidor,
+ingresar_nombre_administrador
 )
 
 print("------Sistema de Diagnóstico y Configuración de Servidor Basado en Reglas------")
-#ingresar numeros 
+
 uso_cpu = ingresar_uso_cpu
 uso_ram = ingresar_uso_ram
 espacio_libre = ingresar_espacio_libre
 usuarios_conectados = ingresar_usuarios_conectados
 procesos_activos = ingresar_procesos_activos
 
-#ingresar opciones EUTILIZACION DE LA FUNCION POR PARAMETROS PARA PEDIR OPCIONES CERRADAS
 sistema_operativo = ingresar_sistema_operativo
 estado_firewall = ingresar_estado_firewall
 tipo_servidor = ingresar_tipo_servidor
+
+nombre_servidor=ingresar_nombre_servidor
+nombre_administrador=ingresar_nombre_administrador
