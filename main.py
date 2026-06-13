f42114702d787cc5529ce61c83cb6fa35fcc3ead
lourdes-privitera <lulitaprivi@gmail.com>
2026-06-13 01:51:12 -0300

    Agrego el llamado a la ejecucion del programa

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
-#liampiar..solo debe quedar la ejecucion del programa
-from inputs import (
-ingresar_uso_cpu,
-ingresar_uso_ram,
-ingresar_espacio_libre,
-ingresar_usuarios_conectados,
-ingresar_procesos_activos,
-ingresar_sistema_operativo,
-ingresar_estado_firewall,
-ingresar_tipo_servidor,
-ingresar_nombre_servidor,
-ingresar_nombre_administrador
-)
+from app import ejecutar_sistema
 
-print("------Sistema de Diagnóstico y Configuración de Servidor Basado en Reglas------")
+ejecutar_sistema()
 
-uso_cpu = ingresar_uso_cpu
-uso_ram = ingresar_uso_ram
-espacio_libre = ingresar_espacio_libre
-usuarios_conectados = ingresar_usuarios_conectados
-procesos_activos = ingresar_procesos_activos
-
-sistema_operativo = ingresar_sistema_operativo
-estado_firewall = ingresar_estado_firewall
-tipo_servidor = ingresar_tipo_servidor
-
-nombre_servidor=ingresar_nombre_servidor
-nombre_administrador=ingresar_nombre_administrador
+'''
+para probar y detectar problemas
+CPU: 95
+RAM: 90
+Disco: 5
+Usuarios: 99
+Procesos: 300
+SO: Windows Server
+Firewall: Inactivo
+Tipo: Web
+'''
+'''
+para probar y sin problemas
+CPU: 20
+RAM: 15
+Disco: 80
+Usuarios: 2
+Procesos: 6
+SO: Windows Server
+Firewall: Activo
+Tipo: Web
+'''
